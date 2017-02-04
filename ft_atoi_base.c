b79b4fc7cf228b299cfd073f72908ddfaea666e5
Vitalii Rybalko <vrybalko@e1r10p4.unit.ua>
2017-02-04 15:44:41 +0200

    refactor

--- ft_atoi_base.c
+++ ft_atoi_base.c
@@ -1,26 +1,42 @@
 /* ************************************************************************** */
-/*																			  */
-/*														  :::	   ::::::::   */
-/*	 ft_atoi_base.c										:+:		 :+:	:+:   */
-/*													  +:+ +:+		  +:+	  */
-/*	 By: vrybalko <marvin@42.fr>					+#+  +:+	   +#+		  */
-/*												  +#+#+#+#+#+	+#+			  */
-/*	 Created: 2017/01/31 16:38:54 by vrybalko		   #+#	  #+#			  */
-/*	 Updated: 2017/01/31 16:38:54 by vrybalko		  ###	########.fr		  */
-/*																			  */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vrybalko <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/04 15:44:07 by vrybalko          #+#    #+#             */
+/*   Updated: 2017/02/04 15:44:11 by vrybalko         ###   ########.fr       */
+/*                                                                            */
 /* ************************************************************************** */
 
-static int	ft_inbase(char c, int base)
+static int	ft_inbase(char c, int base, char ba)
 {
 	if (base <= 10)
 		return (c >= '0' && c <= '9');
-	return ((c >= '0' && c <= '9') || (c >= 'A' && c <= ('A' + base - 10)));
+	return ((c >= '0' && c <= '9') || (c >= ba && c <= (ba + base - 10)));
+}
+
+static	int	check_case(char *str, int base)
+{
+	int		i;
+
+	i = -1;
+	while (str[++i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'A' + base)
+			return (1);
+		if (str[i] >= 'a' && str[i] <= 'a' + base)
+			return (0);
+	}
+	return (1);
 }
 
 int			ft_atoi_base(char *str, int base)
 {
-	int	value;
-	int	sign;
+	int		value;
+	int		sign;
+	char	ba;
 
 	value = 0;
 	if (base < 2 || base > 16)
@@ -29,12 +45,13 @@ int			ft_atoi_base(char *str, int base)
 			|| *str == '\r' || *str == '\v')
 		str++;
 	sign = (*str == '-') ? -1 : 1;
+	ba = check_case(str, base) ? 'A' : 'a';
 	if (*str == '-' || *str == '+')
 		str++;
-	while (ft_inbase(*str, base))
+	while (ft_inbase(*str, base, ba))
 	{
-		if (*str - 'A' >= 0)
-			value = value * base + (*str - 'A' + 10);
+		if (*str - ba >= 0)
+			value = value * base + (*str - ba + 10);
 		else
 			value = value * base + (*str - '0');
 		str++;
