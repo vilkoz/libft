7457f52742f2f986d396bdf8419d5913ade315ea
vilkoz <vilko.a@gmail.com>
2017-03-09 07:39:27 +0200

    add protection to putendl

--- ft_putendl.c
+++ ft_putendl.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putendl.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vrybalko <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vrybalko <vrybalko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/29 14:21:28 by vrybalko          #+#    #+#             */
-/*   Updated: 2016/11/30 21:07:47 by vrybalko         ###   ########.fr       */
+/*   Updated: 2017/03/09 07:38:50 by vrybalko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 
 void	ft_putendl(char const *s)
 {
-	ft_putstr(s);
-	ft_putchar('\n');
+	if (s != NULL)
+	{
+		ft_putstr(s);
+		ft_putchar('\n');
+	}
 }
