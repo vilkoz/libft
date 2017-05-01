b80e7b1b91efbdc817464c1c94a9174d733eafd1
vilkoz <vilko.a@gmail.com>
2017-05-01 15:42:23 +0300

    ft_atod fix

--- ft_atod.c
+++ ft_atod.c
@@ -6,7 +6,7 @@
 /*   By: vrybalko <vrybalko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/23 23:46:52 by vrybalko          #+#    #+#             */
-/*   Updated: 2017/04/24 00:19:22 by vrybalko         ###   ########.fr       */
+/*   Updated: 2017/05/01 15:41:12 by tor              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,13 +42,22 @@ double			ft_atod(const char *s)
 {
 	int		a;
 	int		b;
+	int		is_neg;
+	char	*minus;
+	char	*dot;
 
-	a = ft_atoi(s);
-	if (ft_strchr(s, '.') != NULL)
+	if ((a = ft_atoi(s)) == 0 &&
+			(minus = ft_strchr(s, '-')))
+		is_neg = 1;
+	dot = ft_strchr(s, '.');
+	if (minus && dot && minus > dot)
+		is_neg = 0;
+	if (dot)
 	{
 		b = ft_atoi(ft_strchr(s, '.') + 1);
-		return ((double)a + ((double)b /
-			(double)ft_pow(10, count_nums(ft_strchr(s, '.') + 1))));
+		return (((double)a + (a < 0 ? -1 : 1) * ((double)b /
+			(double)ft_pow(10, count_nums(ft_strchr(s, '.') + 1)))) *
+			(is_neg && a == 0 ? -1. : 1.));
 	}
 	return ((double)a);
 }
